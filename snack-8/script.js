f4120f613ab7a52fc9905b2ca5da53314e186dbf
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-10-30 16:00:04 +0100

    Snack 8

---
+++ snack-8/script.js
@@ -0,0 +1,28 @@
+const students = [
+  {
+    id: 1,
+    name: 'Marco Lanci',
+    age: 32,
+    class: '3C'
+  },
+  {
+    id: 2,
+    name: 'Mario Banfi',
+    age: 34,
+    class: '4A'
+  },
+  {
+    id: 3,
+    name: 'Luigi Banzi',
+    age: 33,
+    class: '5B'
+  },
+];
+
+// Recupera la classe dello studente 'Marco Lanci'
+
+const classLanci = students.find((student) => student.name === 'Marco Lanci').class
+console.log(classLanci)
+
+
+// Risultato: '3C'
